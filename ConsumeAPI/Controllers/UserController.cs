f4d5716e251f7f153eb2760519555045354e36b4
Bishal Niraula <bishalniraula885@gmail.com>
2023-08-23 05:13:49 -0700

    Consume AP | #master

---
+++ ConsumeAPI/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using ConsumeAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ConsumeAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        Uri baseAddress = new Uri("http://localhost:1475/api");
+        HttpClient client;
+        public UserController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = baseAddress;
+        }
+        public ActionResult Index()
+        {
+            List<UserViewModel> users = new List<UserViewModel>();
+            HttpResponseMessage response = client.GetAsync(client.BaseAddress + "/Account").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data= response.Content.ReadAsStringAsync().Result;
+               users= JsonConvert.DeserializeObject<List<UserViewModel>>(data);
+              
+            }
+            return View(users);
+        }
+    }
+}
---
+++ ConsumeAPI/Models/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsumeAPI.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+      
+
+    }
+}
